1b3748f310308b82335a03343f080e9eb88fe66d
Yraya Bolanos Frasquet <yraya.bf@gmail.com>
2017-06-02 12:12:13 +0100

    Modal window refactored
    Modal window refactored to be displayed from more than one button

--- js/controllers/home.controller.js
+++ js/controllers/home.controller.js
@@ -30,15 +30,17 @@
             // Get the modal
             var modal = document.getElementById('movie-details-modal');
 
-            // Get the button that opens the modal
-            var btn = document.getElementById("myBtn");
+            // Get the buttons that opens the modal
+            var btns = document.getElementsByClassName("cover-button");
 
             // Get the <span> element that closes the modal
             var span = document.getElementsByClassName("close")[0];
 
             // When the user clicks the button, open the modal 
-            btn.onclick = function () {
-                modal.style.display = "block";
+            for (var i=0; i < btns.length; i++){
+                btns[i].onclick = function () {
+                    modal.style.display = "block";
+                }
             }
 
             // When the user clicks on <span> (x), close the modal
